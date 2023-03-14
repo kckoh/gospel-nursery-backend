85489f604b6572c5e614727b92f66f9b69b21139
kckoh <kyungcheolkoh@gmail.com>
2023-03-14 14:55:58 -0700

    chore: change api to v1

--- src/main/java/com/example/backend/controller/ImageController.java
+++ src/main/java/com/example/backend/controller/ImageController.java
@@ -1,11 +1,5 @@
 package com.example.backend.controller;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.example.backend.dto.ImageUrl;
 import com.example.backend.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,18 +9,21 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.Set;
 
+import static org.springframework.http.HttpStatus.OK;
+
 @Tag(name = "Image", description = "swagger Image api")
 @CrossOrigin(origins = "https://gospelnursery.net")
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/v1")
 @RequiredArgsConstructor
 @RestController
 public class ImageController {
@@ -35,7 +32,7 @@ public class ImageController {
 
     final String url = "";
 
-    @Operation(summary = "Hello", description = "demo 조회 메서드입니다.")
+    @Operation(summary = "Hello", description = "Hello 조회 메서드입니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "조회 성공"),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
@@ -46,6 +43,8 @@ public class ImageController {
         return "new CI/CD successful!!!";
     }
 
+
+    @ResponseStatus(OK)
     @GetMapping("/images")
     public Set<String> getImages() {
         return imageService.getImages();
